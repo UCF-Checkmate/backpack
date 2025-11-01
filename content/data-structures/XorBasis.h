2b94befd494c62e8c84d9dda452f2cf20c153368
enwask <enwask@ucf.edu>
2025-10-31 21:46:24 -0400

    MAXBIT -> M

--- content/data-structures/XorBasis.h
+++ content/data-structures/XorBasis.h
@@ -5,23 +5,23 @@
  * Description: Forms a basis of binary vectors, with buildback. get() returns a list of the
  * input values associated with vectors that XOR to v. If v is not in the span, or is the zero
  * vector, an empty vector is returned.
- * Time: O(MAXBIT^2 / 32)
+ * Time: O(M^2 / 32)
  * Status: Tested on https://codeforces.com/gym/105789/problem/B
  */
 #pragma once
 
-const int MAXBIT = 64;
-using B = bitset<MAXBIT>;
+const int M = 64;
+using B = bitset<M>;
 
 template<class T> struct Basis {
-    B basis[MAXBIT], which[MAXBIT];
-    optional<T> vals[MAXBIT];
+    B basis[M], which[M];
+    optional<T> vals[M];
     Basis() { memset(vals, 0, sizeof(vals)); }
 
     bool add(B v, T x) {
         if (v.none()) return false;
         B cur; int ind = -1;
-        for (int i = 0; i < MAXBIT; i++) if (v[i]) {
+        for (int i = 0; i < M; i++) if (v[i]) {
             if(vals[i].has_value())
                 v ^= basis[i], cur ^= which[i];
             else if (ind < 0) ind = i;
@@ -35,12 +35,12 @@ template<class T> struct Basis {
     vector<T> get(B v) const {
         if (v.none()) return {{}};
         B w;
-        for (int i = 0; i < MAXBIT; i++) if (v[i])
+        for (int i = 0; i < M; i++) if (v[i])
             v ^= basis[i], w ^= which[i];
         if (v.any()) return {};
 
         vector<T> res;
-        for (int i = 0; i < MAXBIT; i++)
+        for (int i = 0; i < M; i++)
             if (w[i]) res.push_back(*vals[i]);
         return res;
     }
